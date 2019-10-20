a8d6dc6aaa8dd37993184d40e596eec2c98da896
Eunice <babybeareunice@gmail.com>
2019-10-21 02:29:30 +0800

    Create mathContract.sol

---
+++ mathContract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.21;
+import "github.com/OpenZeppelin?zeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract SafeCounter{
+    using SafeMath for uint;
+    uint private count = 0;
+    function getCount() public constant returns (uint){
+        return count;
+    }
+
+    function increment() public{
+        count = count.add(1);
+    }
+
+    function decrement() public{
+        count = count.sub(1);
+    }
+
+}
