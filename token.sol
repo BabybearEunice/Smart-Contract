af56192d5f2708f31a8d5741e6ea6eb709dadc02
Eunice <babybeareunice@gmail.com>
2019-10-21 14:59:11 +0800

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.21;
+import "github.com/OpenZeppelin?zeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Token{
+    mapping (address => uint256) private balances;
+
+    function getBalance(address _account) public constant returns{
+        return balabces[_account];
+    }
+
+    finction Token(uint256 _initialSupply) public{
+        balabces[msg.sender] = _initialSupply;
+    }
+}
